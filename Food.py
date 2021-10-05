bb415094d879c8393be800299e1618b8253e8411
Wesley <ovvesley@protonmail.com>
2021-10-04 21:16:58 -0300

    Food

---
+++ Food.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+class Food:
+    grocery_id : str
+    name : str
+    manufactory: datetime
+    expiration_date: datetime
+    price: float
+    
+
+    
+    
+    
